d24341119ce76dd5d68e27c37843f073e2c54074
Dmitry <kde97ya@yandex.ru>
2023-11-04 15:03:37 +0300

    Added function 'Actions' into Console_chat.cpp

---
+++ Console_Chat/Console_Chat/Console_Chat.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+int actions()
+{
+    std::cout << "\nВведиде код команды : 0 - регистрация, 1 - логин, 2 - отправить сообщение,\n 3 - сменить пользователя, 4 - общий чат, 5 - приватный чат,\n 6 - список пользователей, q - выход" << std::endl;
+    char* choice= new char;
+    std::cin >> *choice;
+    switch (*choice)
+    {
+        case '0':
+        {
+            std::cout << "\nВыбран символ 0" << std::endl;
+            break;
+        }
+        case '1':
+        {
+            std::cout << "\nВыбран символ 1" << std::endl;
+            break;
+        }
+        case '2':
+        {
+            std::cout << "\nВыбран символ 2" << std::endl;
+            break;
+        }
+        case '3':
+        {
+            std::cout << "\nВыбран символ 3" << std::endl;
+            break;
+        }
+        case '4':
+        {
+            std::cout << "\nВыбран символ 4" << std::endl;
+            break;
+        }
+        case '5':
+        {
+            std::cout << "\nВыбран символ 5" << std::endl;
+            break;
+        }
+        case '6':
+        {
+            std::cout << "\nВыбран символ 6" << std::endl;
+            break;
+        }
+        case 'q':
+        {
+            return 0;
+        }
+    }
+    delete choice;
+    actions();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    actions();
+    return 0;
+}
+
+
